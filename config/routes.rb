f1e1501789f4730bfe10ec3e6036731dc256b38f
Jean Oliveira <jbarbosadeoliveira@academic.rrc.ca>
2018-03-12 23:24:47 -0500

    Setup working partial for customers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  resources :customers, only: [:index]
+
+  root to: 'customers#index'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
